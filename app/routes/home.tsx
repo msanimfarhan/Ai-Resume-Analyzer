8acbcb55e06828b8fac0a65afcb67c85dc7a3e52
MD Sanim Farhan <mdsanimfarhan@gmail.com>
2026-02-16 17:39:00 -0400

    Cretaed resume template

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,23 +14,22 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
     return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
         <Navbar/>
-        <section className="page-heading">
-            <div>
-
+        <section className="main-section">
+            <div className="page-heading py-16">
                 <h1>Track Your Application & Resume Ratings</h1>
                 <h2>Review your submissions and check AI-powered feedback</h2>
             </div>
 
+            {resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map(resume => (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                    ))}
+                </div>
+
+            )}
+
         </section>
-
-        {resumes.length > 0 && (
-            <div className="resume-section">
-                {resumes.map(resume => (
-                    <ResumeCard key={resume.id} resume={resume}/>
-                ))}
-            </div>
-
-        )}
 
 
     </main>
